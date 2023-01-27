29d62feccc99f9d86b74a881511e2cd674f18ea9
Alistair Scott <ali.scott@gmail.com>
2023-01-27 11:20:15 +0000

    test

--- infra/prod/us1/ec2/main.tf
+++ infra/prod/us1/ec2/main.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "t3.xlarge"
 }
+
+module "git-module" {
+  source = "git::https://github.com/infracost/terraform-private-module-example.git"
+
+  name = "my-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+}
