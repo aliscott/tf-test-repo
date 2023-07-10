e60e65e4dcf49032e88a46cff6f6022d01719095
Alistair Scott <ali.scott@gmail.com>
2023-07-10 10:03:53 +0100

    test gp2 2

--- infra/dev/us1/ec2/main.tf
+++ infra/dev/us1/ec2/main.tf
@@ -9,6 +9,10 @@ provider "aws" {
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "t3.large"
+  root_block_device {
+   type = "gp2"
+   size = 40
+  }
 }
 
 resource "aws_ebs_volume" "test" {
